0bc19401d7947b1b6e36f87d857b5b353293bfb7
Vigen Baghdasaryan <vigbagh@gmail.com>
2020-02-13 21:48:38 +0400

    Add bash script to check existance of element in array

---
+++ bash/contains-element.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+array=("yes" "y" "true", "1")
+
+value_to_test="yes 1"
+
+value=`echo "$value_to_test" | tr '[:upper:]' '[:lower:]'`
+
+if containsElement "$value" "${array[@]}"; then
+    echo "contains"
+else
+    echo "not cont"
+fi
