f81e8e4e032a85fc94198fa0bcacfc9d4beaf373
Vigen Baghdasaryan <vigen_b@instigatemobile.com>
2021-08-03 10:36:09 +0400

    Update formatting of the last-commits script

--- bash/last-commits.sh
+++ bash/last-commits.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci %cr" $branch | head -n 1` \\t$branch; done | sort -r
+column -t -s "::" <<< $(for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci::%cr" $branch | head -n 1` ::$branch; done | sort -r)
